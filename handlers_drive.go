f001f1f3b333da39d826a02072150b234e30916f
Ramazan ERBAŞARAN <erbasaran@hotmail.com>
2020-01-05 18:44:48 +0300

    Update handlers_drive.go

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "92467278230-qleqn5u6eii1kg6q01nj43a3dai2ek2q.apps.googleusercontent.com"
-const ClientSecret = "YYitHrXwYfceCCmd93-bNAUb"
+const ClientId = "YOUR_CLIENT_ID"
+const ClientSecret = "YOUR_SECRET_ID"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
